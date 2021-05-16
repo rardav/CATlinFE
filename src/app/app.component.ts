16372b56204536faa547d86550b8d66018e3112b
Rares David <david_rares@yahoo.com>
2021-05-17 00:42:44 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'catlin-fe';
+export class AppComponent implements OnInit{
+  title = 'CATlin';
+  questionnaires: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getQuestionnaires();
+  }
+
+  private getQuestionnaires() {
+    this.http.get('https://localhost:44353/api/Questionnaires').subscribe(response => {
+      this.questionnaires = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
